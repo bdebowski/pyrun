00f87a0a4bfad404392c2f7f80f5621bbf76b8b7
bazyli <bazyli.d@gmail.com>
2023-02-13 21:52:48 -0500

    progress on dispatcher

--- dispatcher.py
+++ dispatcher.py
@@ -1,30 +1,15 @@
 from threading import Lock
-from contextlib import contextmanager
-
-@contextmanager
-def managed_resource(*args, **kwds):
-    # Code to acquire resource, e.g.:
-    resource = acquire_resource(*args, **kwds)
-    try:
-        yield resource
-    finally:
-        # Code to release resource, e.g.:
-        release_resource(resource)
-
-with managed_resource(timeout=3600) as resource:
-    # Resource is released at the end of this block,
-    # even if code in the block raises an exception
-
-class ThreadSafeDict:
-    def __init__(self):
-        self._lock = Lock()
-        self._dict = dict()
-
-    def __call__(self, *args, **kwargs):
-        try:
-            yield self._dict
+from queue import Queue
 
 
 class Dispatcher:
-    def __init__(self, input_queue, ):
+    def __init__(self, job_queue: Queue):
+        self._job_queue = job_queue
+        self._completed_or_running = dict()
+
+    def get_job(self, job_id):
+        pass
+
     def __call__(self, *args, **kwargs):
+        while True:
+            pass
