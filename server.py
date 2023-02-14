4c3b2ba7587cbd8b55e28e3e298a9e96b153b99d
Bazyli Debowski <bazyli.d@gmail.com>
2023-02-14 12:07:14 -0500

    FrontEnd and dispatcher connected

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
+from threading import Thread
+
 from flask import Flask, request
 
+from dispatcher import Dispatcher
+from job import Job
+
+
 app = Flask(__name__)
+dispatcher = Dispatcher()
+Thread(target=dispatcher, daemon=True).start()
 
 
 @app.get("/")
 def get_result():
-    return {
-        "response": "Got GET",
-        "id": request.json["id"]}
+    return dispatcher.get_job_result(request.json["id"]).as_dict()
 
 
 @app.post("/")
 def post_request():
-    return {
-        "response": "Got POST",
-        "id": request.json["id"]}
+    try:
+        dispatcher.put_job(Job(request.json["id"], src_code=request.json["src_code"]))
+    except Exception as e:
+        return e.__repr__()
+    return "OK"
