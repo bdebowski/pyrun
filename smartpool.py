1868519a4cfa7594eca00e4e83f0dc31beac7e4b
bazyli <bazyli.d@gmail.com>
2023-02-17 13:48:31 -0500

    Fixes for SmartPool + added system test

--- smartpool.py
+++ smartpool.py
@@ -28,11 +28,13 @@ class ProcessDiedException(Exception):
 class _ManagedProcess(Process):
     _STOP_COMMAND = "STOP"
 
-    def __init__(self, parent_conn: Connection, child_conn: Connection, worker_fn: Callable):
+    def __init__(self, parent_conn: Connection, child_conn: Connection, worker_fn: Callable, args=(), kwargs={}):
         super().__init__(daemon=True)
         self._parent_conn = parent_conn
         self._child_conn = child_conn
         self._worker_fn = worker_fn
+        self._args = args
+        self._kwargs = kwargs
         self._current_job = None
         self._job_start_time = None
 
@@ -66,10 +68,15 @@ class _ManagedProcess(Process):
         self._parent_conn.send(self._STOP_COMMAND)
 
     def run(self):
+        """
+        Overrides Process.run()
+        This will be run when we call p.start()
+        :return:
+        """
         job = self._child_conn.recv()
         while job != self._STOP_COMMAND:
             try:
-                return_val = self._worker_fn(job.payload)
+                return_val = self._worker_fn(job.payload, *self._args, **self._kwargs)
             except Exception as e:
                 return_val = e
             self._child_conn.send(return_val)
@@ -77,8 +84,33 @@ class _ManagedProcess(Process):
 
 
 class SmartPool:
-    def __init__(self, worker_fn: Callable, num_workers: int = 1, timeout_sec: float = 5.0):
+    """
+    A process pool that maintains a constant population of worker processes under conditions where processes might time out or crash.
+    When a process times out it is removed from the pool and a new process takes its place.  Likewise for when a process crashes.
+    The workers run worker functions that process a payload.  The payloads are submitted as jobs.
+    Jobs are passed to the pool using a queue via put_job() and results (if any) are retrieved using a queue via get_result().
+
+    The SmartPool is intended to be run on its own thread.
+    """
+
+    def __init__(self, worker_fn: Callable, args=(), kwargs={}, num_workers: int = 1, timeout_sec: float = 5.0):
+        """
+        Creates a SmartPool object.  Run the object on its own thread like so:
+
+            smart_pool_thread = Thread(target=SmartPool(*args, **kwargs), daemon=True)
+            smart_pool_thread.start()
+
+        :param worker_fn: This is the worker function that each worker process in the pool will run.
+            Can be any callable object that takes at least one argument; i.e. the job payload.  The return value obtained when calling the object
+            is packaged into the JobResult object that is returned from the pool using get_result().
+        :param args: Optional args to pass to the worker function run by each process.
+        :param kwargs: Optional keyword args to pass to the worker function run by each process.
+        :param num_workers: Number of worker processes to maintain in the pool.
+        :param timeout_sec: Maximum time (in seconds) that each worker can spend on running a particular job.
+        """
         self._worker_fn = worker_fn
+        self._args = args
+        self._kwargs = kwargs
         self._num_workers = num_workers
         self._timeout_sec = timeout_sec
 
@@ -98,7 +130,7 @@ class SmartPool:
 
     def _add_process(self):
         parent_conn, child_conn = Pipe()
-        new_process = _ManagedProcess(parent_conn, child_conn, self._worker_fn)
+        new_process = _ManagedProcess(parent_conn, child_conn, self._worker_fn, self._args, self._kwargs)
         new_process.start()
         self._processes.append(new_process)
 
--- tests/test_smartpool.py
+++ tests/test_smartpool.py
@@ -1,1 +1,63 @@
-pass
+import sys
+sys.path.extend(['D:\\bazyli\\Dropbox\\code\\PycharmProjects\\pyrunner'])  # todo
+
+from threading import Thread
+from multiprocessing import TimeoutError
+import time
+
+from smartpool import SmartPool
+
+RETURN42 = """
+def foo():
+    return 42
+
+return_value_container.value = foo()
+"""
+
+TIMEOUT = """
+def foo():
+    while(True):
+        pass
+
+return_value_container.value = foo()
+"""
+
+CRASH = """
+def foo():
+    raise RuntimeError("foo raised this RuntimeError()")
+
+return_value_container.value = foo()
+"""
+
+
+class ReturnValContainer:
+    def __init__(self):
+        self.value = None
+
+
+def exec_and_return(payload: str, exec_globals={}):
+    exec(payload, exec_globals)
+    time.sleep(1)
+    return exec_globals["return_value_container"].value
+
+
+def run_test():
+    _globals = {"return_value_container": ReturnValContainer()}
+    pool = SmartPool(worker_fn=exec_and_return, kwargs={"exec_globals": _globals}, num_workers=4, timeout_sec=2.0)
+    t = Thread(target=pool, daemon=True)
+    t.start()
+    time.sleep(1)
+    codes = [RETURN42, TIMEOUT, CRASH]
+    required_result_type = [int, TimeoutError, RuntimeError]
+    for i in range(100):
+        pool.put_job(i, codes[i % 3])
+    for i in range(1, 101):
+        if i % 10 == 0:
+            print("Received {}".format(i))
+        job_result = pool.get_result()
+        assert type(job_result.return_val) == required_result_type[job_result.id % 3], "Result type mismatch for result id={}".format(job_result.id)
+    print("DONE")
+
+
+if __name__ == "__main__":
+    run_test()
