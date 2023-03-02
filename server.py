1fa644a24bfe16e4fb584f3548b7a3ac14a98941
bazyli <bazyli.d@gmail.com>
2023-03-02 02:48:43 -0500

    Added typing Types to globals env

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from threading import Thread
 from os import environ
+from typing import AnyStr, AsyncIterable, ByteString, ValuesView, List, MappingView, OrderedDict, DefaultDict, ContextManager, Union, Optional, \
+    ItemsView, Callable, ClassVar, Type, Pattern, NoReturn, AsyncGenerator, Match, Set, Dict, MutableSequence, Generator, Counter, Collection, \
+    Sized, AsyncContextManager, Tuple, Sequence, Deque, Reversible, AbstractSet, Final, Iterator, Mapping, AsyncIterator, Container, MutableSet, \
+    Literal, ChainMap, KeysView, Any, MutableMapping, Iterable, Coroutine, FrozenSet, Awaitable, Hashable
 
 from flask import Flask, request
 
@@ -16,13 +20,74 @@ def exec_and_return(payload: str, _globals={}):
     return _globals["return_value_container"].value
 
 
+exec_globals = {
+    "return_value_container": ReturnValContainer(),
+    "AnyStr": AnyStr,
+    "AsyncIterable": AsyncIterable,
+    "ByteString": ByteString,
+    "ValuesView": ValuesView,
+    "List": List,
+    "MappingView": MappingView,
+    "OrderedDict": OrderedDict,
+    "DefaultDict": DefaultDict,
+    "ContextManager": ContextManager,
+    "Union": Union,
+    "Optional": Optional,
+    "ItemsView": ItemsView,
+    "Callable": Callable,
+    "ClassVar": ClassVar,
+    "Type": Type,
+    "Pattern": Pattern,
+    "NoReturn": NoReturn,
+    "AsyncGenerator": AsyncGenerator,
+    "Match": Match,
+    "Set": Set,
+    "Dict": Dict,
+    "MutableSequence": MutableSequence,
+    "Generator": Generator,
+    "Counter": Counter,
+    "Collection": Collection,
+    "Sized": Sized,
+    "AsyncContextManager": AsyncContextManager,
+    "Tuple": Tuple,
+    "Sequence": Sequence,
+    "Deque": Deque,
+    "Reversible": Reversible,
+    "AbstractSet": AbstractSet,
+    "Final": Final,
+    "Iterator": Iterator,
+    "Mapping": Mapping,
+    "AsyncIterator": AsyncIterator,
+    "Container": Container,
+    "MutableSet": MutableSet,
+    "Literal": Literal,
+    "ChainMap": ChainMap,
+    "KeysView": KeysView,
+    "Any": Any,
+    "MutableMapping": MutableMapping,
+    "Iterable": Iterable,
+    "Coroutine": Coroutine,
+    "FrozenSet": FrozenSet,
+    "Awaitable": Awaitable,
+    "Hashable": Hashable
+}
+
+
 pool = SmartPool(
     worker_fn=exec_and_return,
-    kwargs={"_globals": {"return_value_container": ReturnValContainer()}},
+    kwargs={"_globals": exec_globals},
     num_workers=int(environ["PYRUNNER_NUM_WORKERS"]),
     timeout_sec=float(environ["PYRUNNER_TIMEOUT_SEC"]),
     maxmem_bytes=int(environ["PYRUNNER_MAXMEM_MB"])*1024*1024)
 app = Flask(__name__)
+
+
+@app.get("/Config")
+def get_server_params():
+    return {
+        "num_workers": int(environ["PYRUNNER_NUM_WORKERS"]),
+        "timeout_sec": float(environ["PYRUNNER_TIMEOUT_SEC"]),
+        "maxmem_mb": int(environ["PYRUNNER_MAXMEM_MB"])}
 
 
 @app.get("/")
